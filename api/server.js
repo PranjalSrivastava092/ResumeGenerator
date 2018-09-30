adf8dad71f8e715479054c03f51acca33ca5e001
Oshin99 <Oshin0899@gmail.com>
2018-09-30 17:10:11 +0530

    basic server structure

--- api/server.js
+++ api/server.js
@@ -1,4 +1,23 @@
-var express = require('express');
-var mongoose = require('mongoose');
+require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const auth = require('middlewares/auth');
+const errorHandler = require('middlewares/error-handler');
 
-var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(jwt());
+
+app.use('/users', require('./users/controller'));
+app.use('/doc', require('./doc/controller'));
+
+app.use(errorHandler);
+
+const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+const server = app.listen(port, () => {
+    console.log('Server listening on port ' + port);
+});
